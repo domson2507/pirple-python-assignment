2a13d1e5f527a8b3a42b35d1392fd108ff83636c
domson2507 <yawtweeny25@gmail.com>
2019-11-23 17:15:30 +0000

    Assignment to create favorite song

---
+++ main.py
@@ -0,0 +1,27 @@
+"""This is a javascript code displaying the attribute
+of my favorite song"""
+
+artist = "Shatta Wale"
+songTitle = "Enter the Net"
+genre = "Dance Hall"
+durationInSeconds = 240;
+numberOfAwardsWon = 1;
+yearOfRelease = 2018;
+albumName = "single"
+
+print(artist);
+print(songTitle);
+print(genre);
+print(durationInSeconds);
+print(numberOfAwardsWon);
+print(yearOfRelease);
+print(albumName);
+
+
+#artist describes the artist who sang my favorite song
+#songTitle describe the song title of my favorite song
+#genre describes the genre of my favorite song
+#durationInSeconds describes how long the song takes when being played in seconds
+#numberOfAwrds decribes the number of awards won
+#yearOfRelease displays the year the song was released
+#albumName describes the album in which my favorite song can be found
